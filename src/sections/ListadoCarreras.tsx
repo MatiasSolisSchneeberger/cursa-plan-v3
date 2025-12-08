ffdf18ea54bceef44ceddec05a2c0ebc626e31a5
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-12-08 20:54:49 -0300

    Create ListadoCarreras.tsx

---
+++ src/sections/ListadoCarreras.tsx
@@ -0,0 +1,8 @@
+export default function ListadoCarreras() {
+	return (
+		<section className="flex flex-col gap-2">
+			<h1 className="texto-headline text-primary-600 dark:text-primary-400">Carreras:</h1>
+			<article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2"></article>
+		</section>
+	)
+}
