9665a85004528a8ac4bde7034760712c5a2e0d27
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-12-07 21:16:15 -0300

    archivos root

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Layout from "./layout/Layout"
+import {PaginaCarrera} from "./pages/PaginaCarrera" // Tu componente de página
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* Rutas generales (Home, Contacto) sin tema específico */}
+				<Route path="/" element={<Layout />}>
+					{/* --- AQUÍ ESTÁ LA MAGIA --- */}
+					{/* Definimos la ruta padre con el parámetro :carrera */}
+					<Route path="/carrera/:carrera" element={<PaginaCarrera />} />
+					<Route path="/mesas-examenes" element={<div> mesa de examenes</div>} />
+					<Route path="/calendario-academico" element={<div> calendario academico</div>} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
