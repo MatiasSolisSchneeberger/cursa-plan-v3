7c704f5949edb5394c90b98d6d651384e4659a8a
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-12-10 14:25:06 -0300

    cambiando carrera por carreraSlug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import Layout from "./layout/Layout"
 import {PaginaCarrera} from "./pages/PaginaCarrera" // Tu componente de página
 import {Index} from "./pages/Index"
@@ -13,7 +13,7 @@ function App() {
 					<Route index element={<Index />} />
 					{/* --- AQUÍ ESTÁ LA MAGIA --- */}
 					{/* Definimos la ruta padre con el parámetro :carrera */}
-					<Route path="/carrera/:carrera" element={<PaginaCarrera />} />
+					<Route path="/carreras/:carreraSlug" element={<PaginaCarrera />} />
 					<Route path="/mesas-examenes" element={<div> mesa de examenes</div>} />
 					<Route path="/calendario-academico" element={<div> calendario academico</div>} />
 					<Route path="*" element={<Navigate to="/404" />} />
