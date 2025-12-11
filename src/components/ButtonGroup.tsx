69ec941733d3c344f9503f2d98f5869b2fbf2597
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-12-11 11:00:38 -0300

    Creando ButtonGroup

---
+++ src/components/ButtonGroup.tsx
@@ -0,0 +1,7 @@
+export default function ButtonGroup({children}: {children: React.ReactNode}) {
+	return (
+		<section className="flex flex-row items-center justify-center self-stretch shrink-0 relative lg:overflow-visible w-min h-min *:rounded-none *:first:rounded-l-xl *:last:rounded-r-xl *:not-disabled:hover:rounded-none *:first:not-disabled:hover:rounded-l-2xl *:last:not-disabled:hover:rounded-r-2xl">
+			{children}
+		</section>
+	)
+}
