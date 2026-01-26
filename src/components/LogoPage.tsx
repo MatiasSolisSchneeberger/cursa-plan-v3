3631799ccd745ef32768696948248c90f3f087c6
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2026-01-26 00:55:49 -0300

    Update LogoPage.tsx

--- src/components/LogoPage.tsx
+++ src/components/LogoPage.tsx
@@ -5,12 +5,12 @@ export default function LogoPage() {
 	return (
 		<Link
 			to="/"
-			className="bg-primary-400 text-primary-950 dark:bg-primary-600 dark:text-primary-50 rounded-2xl p-2.5 flex flex-row gap-2.5 items-center justify-center self-stretch shrink-0 relative overflow-hidden w-min h-min"
+			className="bg-primary-400 text-primary-950 dark:bg-primary-600 dark:text-primary-50 rounded-2xl p-2.5 flex flex-row gap-2.5 items-center justify-center self-stretch shrink-0 relative overflow-hidden h-min"
 			id="logo">
 			<IconSchool size="22" />
-			<p className="relative flex items-center justify-center text-center font-primary text-sm font-bold tracking-wide">
+			<span className="relative flex items-center justify-center text-center font-primary text-sm font-bold tracking-wide">
 				CursaPlan
-			</p>
+			</span>
 		</Link>
 	)
 }
