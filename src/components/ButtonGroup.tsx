18e2632585dfd0c7ef9562a88f64961313586644
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2026-02-09 01:00:50 -0300

    hacer scroll horizontal

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -1,10 +1,6 @@
 export default function ButtonGroup({children}: {children: React.ReactNode}) {
 	return (
-		<section
-			className="flex flex-row items-center justify-center self-stretch shrink-0 relative lg:overflow-visible h-min 
-        *:rounded-none *:not-last:border-r-0
-        *:first:rounded-l-xl *:first:not-disabled:hover:rounded-l-2xl 
-        *:last:rounded-r-xl *:not-disabled:hover:rounded-none *:last:not-disabled:hover:rounded-r-2xl">
+		<section className="flex flex-row justify-start md:justify-center self-stretch shrink-0 relative lg:overflow-visible h-min *:rounded-none *:first:rounded-l-xl *:first:not-disabled:hover:rounded-l-2xl *:last:rounded-r-xl *:not-disabled:hover:rounded-none *:last:not-disabled:hover:rounded-r-2xl overflow-y-scroll scroll-smooth">
 			{children}
 		</section>
 	)
