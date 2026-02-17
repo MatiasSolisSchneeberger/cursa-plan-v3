b6601d4e65c2f670065206ee8eec259fbb0216ab
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2026-02-17 11:34:23 -0300

    Creando BentoGrid para pagina principal

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,11 @@
-import CarrerasFav from "../sections/CarrerasFav"
+import BentoGrid from "../sections/BentoGrid"
 import HeroSection from "../sections/HeroSection"
-import ListadoCarreras from "../sections/ListadoCarreras"
 
 export function Index() {
 	return (
 		<section className="w-full min-h-screen flex flex-col items-center gap-12">
 			<HeroSection />
-			<CarrerasFav />
-			<ListadoCarreras />
+			<BentoGrid />
 		</section>
 	)
 }
