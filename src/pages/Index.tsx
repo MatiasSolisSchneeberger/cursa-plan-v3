35a98a3090e304f2ccdc22b7b8b6af4a347b4647
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2026-02-18 15:54:06 -0300

    agregando carrusel de carreras al index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import BentoGrid from "../sections/BentoGrid"
+import CarruselCarreras from "../sections/CarruselCarreras"
 import HeroSection from "../sections/HeroSection"
 
 export function Index() {
@@ -6,6 +7,7 @@ export function Index() {
 		<section className="w-full min-h-screen flex flex-col items-center gap-12">
 			<HeroSection />
 			<BentoGrid />
+			<CarruselCarreras />
 		</section>
 	)
 }
