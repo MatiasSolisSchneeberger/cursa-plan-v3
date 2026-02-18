2bbbf86dfcbbb3a7bcc103cb814bd492763d5085
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2026-02-18 15:53:42 -0300

    creando Carrusel de carreras

--- src/components/CardCarrera.tsx
+++ src/components/CardCarrera.tsx
@@ -1,48 +1,66 @@
-import {Link} from "react-router-dom"
-import IconCarrera from "./IconCarrera"
+import { Link } from "react-router-dom";
+import IconCarrera from "./IconCarrera";
+import { cn } from "../utils/cn";
 
 export default function CardCarrera({
 	icon,
 	carrera,
 	slug,
 	link,
+	className,
+	onClick,
 }: {
-	icon: string
-	carrera: string
-	slug: string
-	link?: string
+	icon: string;
+	carrera: string;
+	slug: string;
+	link?: string;
+	className?: string;
+	onClick?: () => void;
 }) {
 	return (
 		<Link
 			to={link || `/carreras/${slug}`}
-			onClick={() => {
-				window.scrollTo({top: 0, behavior: "smooth"})
-			}}
-			className={`theme-${slug} hover:shadow-1 bg-primary-200 dark:bg-primary-950 relative flex h-min w-full flex-1 flex-row items-center justify-between overflow-hidden rounded-4xl py-3 pl-3 pr-9 outline-2 focus:outline-primary-600 transition-all hover:shadow-md duration-100 ease-in-out outline-primary-400 hover:scale-[1.01] gap-3 hover:bg-primary-100 dark:hover:bg-primary-900`}>
-			<div className="relative flex h-10 w-10 shrink-0 flex-row items-center justify-center gap-2.5 rounded-full bg-primary-600 dark:bg-primary-400 p-3">
-				<label className="relative flex items-center justify-center text-center text-primary-50 dark:text-primary-950">
+			onClick={
+				onClick &&
+				(() => {
+					window.scrollTo({ top: 0, behavior: "smooth" });
+				})
+			}
+			className={cn(
+				"hover:shadow-1 bg-primary-200 dark:bg-primary-950 focus:outline-primary-600 outline-primary-400 hover:bg-primary-100 dark:hover:bg-primary-900 relative flex h-min flex-row items-center gap-3 overflow-hidden rounded-4xl py-3 pr-9 pl-3 outline-2 transition-all duration-100 ease-in-out hover:scale-[1.01] hover:shadow-md",
+				slug && `theme-${slug}`,
+				className,
+			)}
+		>
+			{/* Icono */}
+			<div className="bg-primary-600 dark:bg-primary-400 relative flex h-10 w-10 shrink-0 flex-row items-center justify-center gap-2.5 rounded-full p-3">
+				<label className="text-primary-50 dark:text-primary-950 relative flex items-center justify-center text-center">
 					<IconCarrera icon={icon} />
 				</label>
 			</div>
 
-			<h2 className="texto-headline relative flex w-full items-center justify-center self-stretch text-center text-primary-700 dark:text-primary-300">
-				{carrera.replace("Licenciatura", "Lic.").replace("Ingeniería", "Ing.").replace("Profesorado", "Prof.")}
+			{/* Nombre de carrera */}
+			<h2 className="texto-headline text-primary-700 dark:text-primary-300 relative flex w-full items-center justify-center self-stretch text-center">
+				{carrera
+					.replace("Licenciatura", "Lic.")
+					.replace("Ingeniería", "Ing.")
+					.replace("Profesorado", "Prof.")}
 			</h2>
 		</Link>
-	)
+	);
 }
 
 // Skeleton
 export function CardCarreraSkeleton() {
 	return (
-		<div className="animate-pulse bg-primary-200 dark:bg-primary-950 relative flex h-min w-full flex-1 flex-row items-center justify-between overflow-hidden rounded-4xl py-3 pl-3 pr-9 outline-2 transition-all duration-100 ease-in-out outline-primary-400 gap-3">
+		<div className="bg-primary-200 dark:bg-primary-950 outline-primary-400 relative flex h-min w-full flex-1 animate-pulse flex-row items-center justify-between gap-3 overflow-hidden rounded-4xl py-3 pr-9 pl-3 outline-2 transition-all duration-100 ease-in-out">
 			{/* Círculo del icono (simulado) */}
-			<div className="h-10 w-10 shrink-0 rounded-full bg-primary-300 dark:bg-primary-800" />
+			<div className="bg-primary-300 dark:bg-primary-800 h-10 w-10 shrink-0 rounded-full" />
 
 			{/* Barra de texto (simulada) */}
 			<div className="flex w-full items-center justify-center">
-				<div className="h-4 rounded bg-primary-300 dark:bg-primary-800" />
+				<div className="bg-primary-300 dark:bg-primary-800 h-4 rounded" />
 			</div>
 		</div>
-	)
+	);
 }
---
+++ src/sections/CarruselCarreras.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import supabase from "../utils/supabase";
+import CardCarrera from "../components/CardCarrera";
+import { Link } from "react-router-dom";
+import Cargando from "./Cargando";
+
+interface Carreras {
+	id: number;
+	nombre: string;
+	slug: string;
+	icon: string;
+}
+
+export default function CarruselCarreras() {
+	const [loading, setLoading] = useState(true);
+	const [carreras, setCarreras] = useState<Carreras[]>([]);
+
+	useEffect(() => {
+		const fetchCarreras = async () => {
+			const { data, error } = await supabase
+				.from("carreras")
+				.select("id,nombre,slug,icon");
+			if (error) {
+				console.error("Error al obtener las carreras:", error);
+				return;
+			}
+			setCarreras(data as Carreras[]);
+			setLoading(false);
+		};
+
+		fetchCarreras();
+	}, []);
+
+	if (loading) {
+		return <Cargando />;
+	}
+
+	return (
+		<section className="container mx-auto flex flex-col items-center gap-3">
+			<span className="texto-label text-primary-400 dark:text-primary-600">
+				Carreras disponibles
+			</span>
+			<Link
+				to="/explorar"
+				onClick={() => {
+					window.scrollTo({
+						top: 0,
+						behavior: "smooth",
+					});
+				}}
+				className="w-full"
+			>
+				<h2 className="texto-headline text-text-800 dark:text-text-200 text-center">
+					Todas las carreras de FaCENA
+				</h2>
+				<p className="text-text-600 dark:text-text-400 texto-title text-center text-pretty">
+					Busca tu carrera y deja de perder tiempo buscando la
+					información importante entre tantas resoluciones
+				</p>
+				<section className="relative w-full overflow-hidden py-4">
+					<div className="animate-scroll flex w-max gap-8 hover:[animation-play-state:paused]">
+						{[...carreras, ...carreras].map(
+							({ nombre, slug, icon }, index) => (
+								<CardCarrera
+									carrera={nombre}
+									slug={slug}
+									icon={icon}
+									key={index}
+									className="w-max *:text-nowrap"
+								/>
+							),
+						)}
+					</div>
+				</section>
+			</Link>
+		</section>
+	);
+}
