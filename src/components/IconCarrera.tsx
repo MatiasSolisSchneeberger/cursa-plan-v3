7e9cd125132f752f88dfe70b9945e6aaf162e73f
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2026-02-21 21:38:59 -0300

    cambio de icono Agrimensura

--- src/components/IconCarrera.tsx
+++ src/components/IconCarrera.tsx
@@ -1,6 +1,5 @@
 import {
 	IconMicroscope,
-	IconRuler2,
 	IconBolt,
 	IconRobot,
 	IconSeedling,
@@ -14,39 +13,48 @@ import {
 	IconPrismLight,
 	IconFlask2Filled,
 	IconBook2,
-} from "@tabler/icons-react"
+	IconGeometry,
+} from "@tabler/icons-react";
 
-export default function IconCarrera({icon, className, size}: {icon: string; className?: string; size?: number}) {
+export default function IconCarrera({
+	icon,
+	className,
+	size,
+}: {
+	icon: string;
+	className?: string;
+	size?: number;
+}) {
 	switch (icon) {
 		case "microscope":
-			return <IconMicroscope size={size} className={className} />
-		case "ruler-2":
-			return <IconRuler2 size={size} className={className} />
+			return <IconMicroscope size={size} className={className} />;
+		case "geometry":
+			return <IconGeometry size={size} className={className} />;
 		case "bolt":
-			return <IconBolt size={size} className={className} />
+			return <IconBolt size={size} className={className} />;
 		case "robot":
-			return <IconRobot size={size} className={className} />
+			return <IconRobot size={size} className={className} />;
 		case "seedling":
-			return <IconSeedling size={size} className={className} />
+			return <IconSeedling size={size} className={className} />;
 		case "atom":
-			return <IconAtom size={size} className={className} />
+			return <IconAtom size={size} className={className} />;
 		case "flask":
-			return <IconFlask size={size} className={className} />
+			return <IconFlask size={size} className={className} />;
 		case "math":
-			return <IconMath size={size} className={className} />
+			return <IconMath size={size} className={className} />;
 		case "device-imac":
-			return <IconDeviceImac size={size} className={className} />
+			return <IconDeviceImac size={size} className={className} />;
 		case "butterfly":
-			return <IconButterfly size={size} className={className} />
+			return <IconButterfly size={size} className={className} />;
 		case "telescope":
-			return <IconPrismLight size={size} className={className} />
+			return <IconPrismLight size={size} className={className} />;
 		case "ruler":
-			return <IconRuler size={size} className={className} />
+			return <IconRuler size={size} className={className} />;
 		case "test-pipe":
-			return <IconTestPipe size={size} className={className} />
+			return <IconTestPipe size={size} className={className} />;
 		case "flask-2-filled":
-			return <IconFlask2Filled size={size} className={className} />
+			return <IconFlask2Filled size={size} className={className} />;
 		default:
-			return <IconBook2 size={size} className={className} />
+			return <IconBook2 size={size} className={className} />;
 	}
 }
